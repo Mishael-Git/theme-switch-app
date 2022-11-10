d1f1ea417fc19cc32078df9ef7086e138ec9382c
Mishael <mishaelclifford1@gmail.com>
2022-11-10 16:13:37 +0100

    Navbar test completed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,22 +3,24 @@ import { GiMeditation } from "react-icons/gi";
 import { CgProfile, CgDetailsMore } from "react-icons/cg";
 function Navbar() {
   return (
-    <div className="flex items-center bg-slate-100 space-x-11 ">
-      <div class="flex bg-slate-400">
+    <div className="flex items-center bg-slate-100 space-x-11 h-6 ">
+      <div class="flex bg-slate-400  ">
         <GiMeditation size={20} />
         <p class="font-thin">BREEZ</p>
       </div>
-      <div class="flex  justify-between">
+      <div class="flex pl-96 space-x-14">
+      <div class="flex pl-64">
         <CgProfile size={20} />
         <p>Login</p>
       </div>
 
-      <div class="bg-[#f5af9ee0] rounded-full">
+      <div class="bg-[#f5af9ee0] rounded-full ">
         <button>Switch-Theme</button>
       </div>
 
-      <div class="bg-slate-900">
-        <CgDetailsMore size={20} class="text-slate-200" />
+      <div class="bg-slate-900 ">
+        <CgDetailsMore size={20} class="text-slate-200 " />
+      </div>
       </div>
     </div>
   );
